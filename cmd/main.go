9657e7b3adce016ebcf20413e2f38cd7b278b896
dacd <kirshul07@gmail.com>
2026-06-27 19:09:59 +0300

    фикс CORS

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,7 @@ import (
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "https://event-registration.2090.fun")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
